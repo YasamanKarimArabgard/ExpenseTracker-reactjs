1faefcb597293f63d6c0932d2d26b04748083d59
jasminearabgard <yasaman.arabgard@outlook.com>
2022-02-16 16:19:40 +0330

    add component

--- src/Components/OverViewComponnent/OverViewComponent.jsx
+++ src/Components/OverViewComponnent/OverViewComponent.jsx
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
 import TransActionForm from '../TransActionForm/TransActionForm';
 
-const OverViewComponent = ({ income, expense , addTransAction}) => {
+const OverViewComponent = ({ income, expense, addTransAction }) => {
 
     const [isShow, setIsShow] = useState(false);
 
-    return (
-        <>
-            <div className='col-12 d-flex justify-content-between mt-1 align-items-center'>
-                <h5 className='text-secondary mx-2'>balanse : {income - expense}</h5>
-                <button className={`btn btn-sm h-50 ${isShow ? 'btn-outline-danger' : 'btn-primary'}`}
-                    onClick={() => setIsShow(prevState => !prevState)}>
-                    {isShow ? 'Cancel' : 'Add'}
-                    </button>
-            </div>
-            {isShow && <TransActionForm addTransAction={addTransAction} setIsShow={setIsShow}/>}
-            <div className='col-12 d-flex flex-column my-2'>
-                <div className='col-12 d-flex justify-content-between'>
-                   <div className='col-6 border rounded p-2 d-flex justify-content-between'>
-                     <h6>Income</h6>
-                     <h6 className='text-success'>{income}$</h6>
-                   </div>
-                   <div className='col-6 border rounded p-2 d-flex justify-content-between'>
-                     <h6>Expense</h6>
-                     <h6 className='text-danger'>{expense}$</h6>
-                   </div>
+return (
+    <>
+        <div className='col-12 d-flex justify-content-between mt-1 align-items-center'>
+            <h5 className='text-secondary mx-2'>Balanse : {income - expense}</h5>
+            <button className={`btn btn-sm h-50 ${isShow ? 'btn-outline-danger' : 'btn-primary'}`}
+                onClick={() => setIsShow(prevState => !prevState)}>
+                {isShow ? 'Cancel' : 'Add'}
+            </button>
+        </div>
+        {isShow && <TransActionForm addTransAction={addTransAction} setIsShow={setIsShow} />}
+        <div className='col-12 d-flex flex-column my-2'>
+            <div className='col-12 d-flex justify-content-between'>
+                <div className='col-6 border rounded p-2 d-flex justify-content-between'>
+                    <h6>Income</h6>
+                    <h6 className='text-success'>{income}$</h6>
+                </div>
+                <div className='col-6 border rounded p-2 d-flex justify-content-between'>
+                    <h6>Expense</h6>
+                    <h6 className='text-danger'>{expense}$</h6>
                 </div>
             </div>
-        </>
-    );
+        </div>
+    </>
+);
 };
 
 export default OverViewComponent;
